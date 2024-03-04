625461c7224b104bed1aacb0ae022ccb3af37cf3
ulzptrzz <auliaptr426@gmail.com>
2024-03-04 18:14:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'testimonial': "url('/Images/bg-testimonial.png')",
+        },
+      },
+    },
+    plugins: [],
+  }
